843fd5af3541e365167beb4378f4bbfa8b0042b3
Scott M. Barbour <smbarbour@gmail.com>
2016-07-23 02:21:07 -0500

    Implement Deep Resonance Radiation handling

---
+++ src/main/java/com/mcupdater/mods/igideepresonance/TagDeepResonance.java
@@ -0,0 +1,42 @@
+package com.mcupdater.mods.igideepresonance;
+
+import com.github.lunatrius.ingameinfo.tag.Tag;
+import com.github.lunatrius.ingameinfo.tag.registry.TagRegistry;
+import elec332.core.world.WorldHelper;
+import mcjty.deepresonance.items.RadiationMonitorItem;
+import mcjty.lib.varia.GlobalCoordinate;
+import mcjty.rftoolsdim.dimensions.DimensionInformation;
+import mcjty.rftoolsdim.dimensions.DimensionStorage;
+import mcjty.rftoolsdim.dimensions.RfToolsDimensionManager;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
+public abstract class TagDeepResonance extends Tag
+{
+	@Override
+	public String getCategory() {
+		return "deepresonance";
+	}
+
+	public static class DeepResonanceRadiation extends TagDeepResonance {
+		@Override
+		public String getValue() {
+			try {
+				RadiationMonitorItem.fetchRadiation(player);
+				return String.valueOf(new Float(RadiationMonitorItem.radiationStrength).longValue());
+			} catch (NullPointerException npe) {
+				return "0";
+			} catch (Throwable e) {
+				log(this, e);
+			}
+			return "NaN";
+		}
+	}
+
+	public static void register() {
+		TagRegistry.INSTANCE.register(new DeepResonanceRadiation().setName("drradiation"));
+	}
+
+	public void log(Tag tag, Throwable ex) {
+		FMLCommonHandler.instance().getFMLLogger().warn(IGIDeepResonance.metadata.modId + ":" + tag.getName(), ex);
+	}
+}
